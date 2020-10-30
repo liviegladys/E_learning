470208bf1d637dc3cfc2ca6c6f00e4564040049b
liviegladys <gladys.akela.pro@gmail.com>
2020-10-30 11:00:39 +0100

    second commit

--- backend/src/origami.js
+++ backend/src/origami.js
@@ -6,6 +6,7 @@ const OrigamiSchema= mongoose.Schema({
     OrigamiName:{type:String,},
     OrigamiPic:{type:String, default: 'boite.jpg'},
     OrigamiDiag:{type:String,default: 'boite.jpg'},
+    OrigamiCate:{type:String,enum:['animaux','fleur','']}
     
 });
  
